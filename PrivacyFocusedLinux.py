a203185f4b384595f36dafa2eb5e7a42d9a562ea
Zethius 'Skengman' Redacted <67869991+zethiusRedacted@users.noreply.github.com>
2020-07-10 01:10:27 +1000

    Update PrivacyFocusedLinux.py

--- PrivacyFocusedLinux.py
+++ PrivacyFocusedLinux.py
@@ -8,29 +8,12 @@ import os
 import time
 #displaying time for syslog
 from datetime import datetime as dt
-
-#asking the user if they are using ubuntu-minimal
-print("Are you running Ubuntu-Minimal? (Y/N)")
-ques = input()
-ques.lower()
-
-if ques == "yes" or ques == "y":
-	#starting the program if they are
-	start_prog()
-elif ques == "no" or ques == "n":
-	#asking them to install it if they are not
-	time.sleep(1)
-	print("\nUnfortunately, this program was designed for Ubuntu-Minimal.")
-	print("Please re-install Ubuntu for maximum privacy.\n")
-	input()
-else:
-	input("\n")
 	
 def aClean():
 	 
 	 #autocleans and autoremoves files
 	 print("Removing...")
-	 os.system("sudo apt-get autoremove")
+	 os.system("sudo apt-get -y autoremove")
 	 time.sleep(1)
 	 
 	 print("\nCleaning...")
@@ -41,7 +24,10 @@ def aClean():
 	 print("YOU HAVE BEEN...PRIVATISED ( ͡° ͜ʖ ͡°)")
 	 print("-"*50)
 	 
+	 print("Press Enter to reboot...")
 	 input()
+	 os.system("sudo reboot")
+	 
 	 return()
 	
 def privInstall():
@@ -50,53 +36,53 @@ def privInstall():
 	
 	#mat (metadata anonymisation tool) deletes metadata from files
 	print("""Metadata Anonymisation Tool (i.e., MAT) removes metadata from images, documents and other types of files.""")
-	os.system("sudo apt-get install mat")
+	os.system("sudo apt-get install -y mat")
 	time.sleep(1)
 	
 	#secure-delete allows you to securely delete files
 	print("""\nSecure-Delete (srm) allows you to securely delete files which disables forensic teams to recover it.""")
-	os.system("sudo apt-get install secure-delete")
+	os.system("sudo apt-get install -y secure-delete")
 	time.sleep(1)
 	
 	#UFW or uncomplicated fire wall
 	print("""\nUncomplicated Firewall blocks all shady traffic in or out of your computer.""")
-	os.system("sudo apt-get install ufw")
+	os.system("sudo apt-get install -y ufw")
 	os.system("sudo ufw enable")
 	time.sleep(1)
 	
 	#HTOP allows you to moniter the ongoing processes
 	print("""\nHTOP allows you to monitor all the ongoing processes on your computer.""")
-	os.system("sudo apt-get install htop")
+	os.system("sudo apt-get install -y htop")
 	time.sleep(1)
 	
 	#locate helps you locate packages/documents with a mere command
 	print("""\nLocate allows you to search for packages, software or documents with one mere command.""")
-	os.system("sudo apt-get install locate")
+	os.system("sudo apt-get install -y locate")
 	time.sleep(1)
 	
 	#MacChanger allows you to change/spoof your MAC address
 	print("""\nMAC Changer allows you to change or spoof your MAC address.""")
-	os.system("sudo apt-get install macchanger")
+	os.system("sudo apt-get install -y macchanger")
 	time.sleep(1)
 	
 	#Nautilus Wipe allows you to delete files securely in a GUI interface
 	print("""\nNautilus Wipe (nautilus-wipe) allows you to securely delete files in a GUI interface to protect them from recovering forensic teams.""")
-	os.system("sudo apt-get install nautilus-wipe")
+	os.system("sudo apt-get install -y nautilus-wipe")
 	time.sleep(1)
 	
 	#ClamAV and #ClamTK helps you search for and eliminate virus/ransomware/malware
 	print("""\nClam Antivirus (clamav) and ClamTK (GUI ClamAV) enables you to scan your computer and eliminate viruses/malware/ransomeware.""")
-	os.system("sudo apt-get install clamav && sudo apt-get install clamtk")
+	os.system("sudo apt-get install -y clamav && sudo apt-get install -y clamtk")
 	time.sleep(1)
 	
 	#KeePassXC is an oper-source, offline, and encrypted password manager
 	print("""\nKeePassXC is an open-source, offline and encrypted password manager which allows you to save password securely.""")
-	os.system("sudo apt-get install keepassxc")
+	os.system("sudo apt-get install -y keepassxc")
 	time.sleep(1)
 	
 	#GTKHash is a GUI hash checker
 	print("""\nGTK Hash allows you to check hash fingerprinting of a file.""")
-	os.system("sudo apt-get install gtkhash")
+	os.system("sudo apt-get install -y gtkhash")
 	time.sleep(1)
 	
 	print("-"*50)
@@ -108,8 +94,8 @@ def privInstall():
 def classicLin():
 
 	print("Disabling Ubuntu Dock...\n")
-	os.system("sudo apt-get purge gnome-shell-extension-ubuntu-dock")
-	time.sleep()
+	os.system("sudo apt-get purge -y gnome-shell-extension-ubuntu-dock")
+	time.sleep(1)
 	
 	privInstall()
 	
@@ -137,31 +123,27 @@ def deSnap():
 	print("Purging Snap completely...\n")
 	time.sleep(1)
 	#removing the snap-store completely
-	os.system("sudo snap remove snap-store")
-	os.system("sudo snap remove gtk-common-themes")
-	os.system("sudo snap remove gnome-3-34-1804
-	os.system("sudo snap remove core18")
-	os.system("sudo apt-get purge snapd")
+	os.system("sudo apt-get purge -y snapd")
 	print("...")
 	print("Done!\n")
 	
 	#as a substitution for a package manager, flatpak will be installed
 	print("Installing Flatpak...\n")
 	time.sleep(1)
-	os.system("sudo apt-get install flatpak")
+	os.system("sudo apt-get install -y flatpak")
 	os.system("flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo")
-	os.system("sudo apt-get install gnome-software-plugin-flatpak")
+	os.system("sudo apt-get install -y gnome-software-plugin-flatpak")
 	
 	#install flatpak plugin installs snapd again so purging it again
-	os.system("sudo apt-get purge gnome-software-plugin-snap")
-	os.system("sudo apt-get purge snapd")
+	os.system("sudo apt-get purge -y gnome-software-plugin-snap")
+	os.system("sudo apt-get purge -y snapd")
 	print("...")
 	print("Done!\n")
 	
 	time.sleep(1)
 	#automatic updates are also a security risk
 	print("Disabling automatic updates...\n")
-	os.system("sudo apt-get purge update-notifier")
+	os.system("sudo apt-get purge -y update-notifier")
 	
 	#ubuntu-dock is not a privacy concern and hence a choice
 	time.sleep(1)
@@ -190,18 +172,34 @@ def start_prog():
 	#running updates and upgrades before the program begins
 	print("Updating")
 	print("-"*50)
-	os.system("sudo apt-get update")
+	os.system("sudo apt-get -y update")
 	time.sleep(1)
 
 	#running a full-upgrade to install upgrades and delete old packages
 	print("-"*50)
 	print("Upgrading")
 	print("-"*50)
-	os.system("sudo apt-get full-upgrade")
+	os.system("sudo apt-get -y full-upgrade")
 	time.sleep(1)
 	
 	#running the deSnapping program
 	deSnap()
 	
 	return()
-start_prog()
+	
+#asking the user if they are using ubuntu-minimal
+print("Are you running Ubuntu-Minimal? (Y/N)")
+ques = input()
+ques.lower()
+
+if ques == "yes" or ques == "y":
+	#starting the program if they are
+	start_prog()
+elif ques == "no" or ques == "n":
+	#asking them to install it if they are not
+	time.sleep(1)
+	print("\nUnfortunately, this program was designed for Ubuntu-Minimal.")
+	print("Please re-install Ubuntu for maximum privacy.\n")
+	input()
+else:
+	input("\n")
